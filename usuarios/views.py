d3e6e3d5649363f273b92409347dc334a7619dcc
Vitor L Kormann <vk273@hotmail.com>
2023-04-13 21:33:27 -0300

    Alterações no views.py

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 # Create your views here.
@@ -10,3 +12,31 @@ def cadastro(request):
         email = request.POST.get('email')
         senha = request.POST.get('senha')
         confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not (senha == confirmar_senha):    
+            return redirect(reverse('cadastro'))
+        
+        #TODO: validar força da senha
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            return redirect(reverse('cadastro'))   
+        
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        user.save()
+        return redirect(reverse('login'))
+    
+from django.contrib.messages import constants
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info ',
+}
+
+from django.contrib import messages
+from django.contrib.messages import constants
+
+messages.add_message(request, constants.ERROR, 'Mensagem')
