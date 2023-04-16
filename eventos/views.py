8a23deacd016a71f8351d1a07f7f6438cb989502
Vitor L Kormann <vk273@hotmail.com>
2023-04-16 16:57:47 -0300

    Cria a lista de exibição de participantes

--- eventos/views.py
+++ eventos/views.py
@@ -4,6 +4,11 @@ from .models import Evento
 from django.urls import reverse
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.http import Http404
+import csv
+from secrets import token_urlsafe
+import os
+from django.conf import settings
 
 @login_required
 def novo_evento(request):
@@ -60,13 +65,35 @@ def inscrever_evento(request, id):
     elif request.method == "POST":
         #Validar se o usuario já é um participante
 
-        evento.partcipantes.add(request.user)
+        evento.participantes.add(request.user)
         evento.save()
 
         messages.add_message(request, constants.SUCCESS, 'Inscrição realizada com sucesso.')
 
         return redirect(f'/eventos/inscrever_evento/{id}/')
-
     
 
-        
+def participantes_evento(request, id):
+    evento = get_object_or_404(Evento, id=id)
+    if not evento.criador == request.user:
+        raise Http404("Esse evento não é seu")
+    if request.method == "GET":
+        participantes = evento.participantes.all()
+        return render(request, 'participantes_evento.html', {'evento': evento, 'participantes': participantes})
+
+def gerar_csv(request, id):
+    evento = get_object_or_404(Evento, id=id)
+    if not evento.criador == request.user:
+        raise Http404('Esse evento não é seu')
+    participantes = evento.participantes.all()
+    
+    token = f'{token_urlsafe(6)}.csv'
+    path = os.path.join(settings.MEDIA_ROOT, token)
+
+    with open(path, 'w') as arq:
+        writer = csv.writer(arq, delimiter=",")
+        for participante in participantes:
+            x = (participante.username, participante.email)
+            writer.writerow(x)
+
+    return redirect(f'/media/{token}')
